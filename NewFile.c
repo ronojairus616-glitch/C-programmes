01a1950ab5b1bde0c278d68f6562ebd31cb219b0
ronojairus616-glitch <ronojairus616@gmail.com>
2026-09-10 11:05:03 +0300

    Add files via upload

---
+++ NewFile.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+int main(){
+	printf("Units offered this semester: \n");
+	printf("1. Introduction to programming \n");
+	printf("2. Data structures \n");
+	printf("3. Computer networks \n");
+	printf("4. Database systems \n");
+	printf("5. Software engineering");
+	return 0;
+	
+}
